b6dc1078caf0de6cf2de6403b555c68cb0c1f3b9
voiys <matej2714@gmail.com>
2023-10-10 10:52:03 +0200

    added superjson and context

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -1,6 +1,15 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
-import type { AppRouter } from '@/server/routers/_app';
+import { createServerSideHelpers } from '@trpc/react-query/server';
+import { appRouter, type AppRouter } from '@/server/routers/_app';
+import superjson from 'superjson';
+import { createContext } from '@/server/trpc';
+
+export const trpcSSR = createServerSideHelpers({
+	router: appRouter,
+	ctx: createContext(),
+	transformer: superjson, // optional - adds superjson serialization
+});
 
 function getBaseUrl() {
 	if (typeof window !== 'undefined')
@@ -18,6 +27,7 @@ function getBaseUrl() {
 export const trpc = createTRPCNext<AppRouter>({
 	config(_opts) {
 		return {
+			transformer: superjson,
 			links: [
 				httpBatchLink({
 					/**
